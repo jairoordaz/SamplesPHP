b71f426b9ab1607e47acb0289d3bf1e743c60a1e
Jairo Ordaz Moreno <ordaz_moreno@live.com.mx>
2019-07-16 09:14:32 -0500

    Update Funciones.php

--- Funciones.php
+++ Funciones.php
@@ -6,5 +6,30 @@ class Funciones(){
 		return $texto;
 	}
 
+	function sumar($numero1,$numero2){
+    	$resultado = $numero1+ $numero2;
+    	return $resultado;
+    }
+
+    function divide($numero1,$numero2){
+    	$resultado = $numero1/$numero2;
+    	return round($resultado);
+    }
+
+    function generarRandom($n1,$n2){
+        $numero  = rand($n1, $n2);
+        return $numero;
+    }
+
+    function mayusculas($texto){
+        $resultado = strtoupper($texto);
+        return $resultado;
+    }
+
+    function minusculas($texto){
+        $resultado = strtolower($texto);
+        return $resultado;
+    }
+
 }
 ?>
